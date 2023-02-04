bb15a06baa6723ab31b2dddfc0a1c4ca71104e21
Bethan <bethan.palmer@digital.cabinet-office.gov.uk>
2023-02-04 09:43:36 +0000

    Add end state for 02_03

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,5 +4,20 @@ public class Calculator {
         return x + y;
     }
 
+    // This method should return the result of subtracting y from x.
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    // This method should return the result of multiplying x and y.
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    // This method should return the result of diving x by y.
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
 
 }
