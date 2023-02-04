ef93855042545676c9874469d7b1da3812fb5e6f
Bethan <bethan.palmer@digital.cabinet-office.gov.uk>
2023-02-04 10:59:41 +0000

    Add begin state for 02_04

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,17 +6,17 @@ public class Calculator {
 
     // This method should return the result of subtracting y from x.
     public int subtract(int x, int y) {
-        return x - y;
+        return 0;
     }
 
     // This method should return the result of multiplying x and y.
     public int multiply(int x, int y) {
-        return x * y;
+        return 0;
     }
 
     // This method should return the result of diving x by y.
     public int divide(int x, int y) {
-        return x / y;
+        return 0;
     }
 
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,31 +15,4 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
-    @Test
-    public void testSubtract() {
-        int expected = 3;
-
-        int actual = calculator.subtract(5, 2);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testMultiply() {
-        int expected = 6;
-
-        int actual = calculator.multiply(2, 3);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testDivide() {
-        int expected = 2;
-
-        int actual = calculator.divide(6, 3);
-
-        assertEquals(expected, actual);
-    }
-
 }
