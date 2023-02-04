171b3034035df7fcbfbe784503f1d038d22d12aa
Bethan <bethan.palmer@digital.cabinet-office.gov.uk>
2023-02-04 09:32:53 +0000

    Add end state for 02_02

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        int expected = 3;
+
+        int actual = calculator.add(1, 2);
+
+        assertEquals(expected, actual);
+    }
+
+}
