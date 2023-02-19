f96f2ffe87c37bb6ba934f74d94bfb3b0004c702
Bethan <bethan.palmer@digital.cabinet-office.gov.uk>
2023-02-19 09:42:12 +0000

    Add content for 03_04

---
+++ src/main/java/HairSalon.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class HairSalon {
+
+    private String[] openingDays = {"Monday", "Tuesday"};
+
+    // This method should return the openingDays array.
+    public String[] getOpeningDays() {
+        return new String[]{};
+    }
+
+    // This method should return true if the weekday passed in is present in the openingDays array.
+    // If not it should return false. If the String passed in does not end in the letters "day",
+    // it should throw an IllegalArgumentException with a message explaining that only weekdays are valid.
+    public boolean isOpen(String weekDay) {
+        return false;
+    }
+
+
+
+
+
+
+}
---
+++ src/test/java/HairSalonTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+class HairSalonTest {
+
+    HairSalon hairSalon = new HairSalon();
+
+    @Test
+    public void testGetOpeningDays() {
+
+    }
+
+    @Test
+    public void testSalonIsOpenOnMonday() {
+
+    }
+
+    @Test
+    public void testSalonIsClosedOnWednesday() {
+
+    }
+
+    @Test
+    public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
+
+    }
+
+}
