5ce2d089049b2e248fb712771017e43a84ba72f7
Bethan <bethan.palmer@digital.cabinet-office.gov.uk>
2023-03-19 17:45:58 +0000

    Add content for 04_04

---
+++ src/main/java/DateChecker.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class DateChecker {
+
+    enum Month {
+        JANUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+    }
+
+    public boolean has31Days(Month month) {
+        return false;
+    }
+
+    
+
+
+}
---
+++ src/test/java/DateCheckerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateCheckerTest {
+
+
+    @Test
+    public void testHas31Days() {
+
+    }
+
+    @Test
+    public void testDoesNotHave31Days() {
+
+    }
+
+}
