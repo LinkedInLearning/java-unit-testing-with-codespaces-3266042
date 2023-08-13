8c4a92d09c30d4890da0b4501d14d6796dd82140
Deepank Kartikey <deepankkartikey@gmail.com>
2023-08-13 19:04:08 +0000

    Parameterized Tests and Enums

--- src/main/java/DateChecker.java
+++ src/main/java/DateChecker.java
@@ -18,10 +18,14 @@ public class DateChecker {
     }
 
     public boolean has31Days(Month month) {
-        return false;
+        if (month.equals(Month.FEBRUARY) ||
+                month.equals(Month.APRIL) ||
+                month.equals(Month.JUNE) ||
+                month.equals(Month.SEPTEMBER) ||
+                month.equals(Month.NOVEMBER)) {
+            return false;
+        }
+        return true;
     }
-
-    
-
 
 }
