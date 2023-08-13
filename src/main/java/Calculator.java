f3809132e4b2d0b04c8cee5451942aab6eb2f1ff
Deepank Kartikey <deepankkartikey@gmail.com>
2023-08-13 16:18:57 +0000

    Added Tests and did TDD

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,18 +6,17 @@ public class Calculator {
 
     // This method should return the result of subtracting y from x.
     public int subtract(int x, int y) {
-        return 0;
+        return x - y;
     }
 
     // This method should return the result of multiplying x and y.
     public int multiply(int x, int y) {
-        return 0;
+        return x * y;
     }
 
     // This method should return the result of diving x by y.
     public int divide(int x, int y) {
-        return 0;
+        return x / y;
     }
-
 
 }
