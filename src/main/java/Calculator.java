ead455d528531a107d1c454c36d358d7d85533fd
Jacob McCullough <34502339+razorwired1124@users.noreply.github.com>
2023-11-04 06:35:56 +0000

    Created first JUnit example using Calculator

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+
+
+  public int sum(int x, int y) {
+    return x + y;
+  }
+
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+  
+  Calculator calculator = new Calculator();
+
+  @Test
+  public void testSum() {
+    int expected = 3;
+
+    int actual = calculator.sum(1, 2);
+
+    assertEquals(expected, actual);
+  }
+}
