bad1c32a123d809bfb32bbce27cc63927d90ad98
Jacob McCullough <34502339+razorwired1124@users.noreply.github.com>
2023-11-20 08:05:48 +0000

    :)

--- src/test/java/DateCheckerTest.java
+++ src/test/java/DateCheckerTest.java
@@ -1,4 +1,4 @@
- import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -11,14 +11,15 @@ class DateCheckerTest {
     DateChecker dateChecker = new DateChecker();
 
     @ParameterizedTest
-    @EnumSource(value = DateChecker.Month.class, names = {"JANUARY", "MARCH", "MAY", "JULY", "AUGUST", "OCTOBER", "DECEMBER"})
+    @EnumSource(value = DateChecker.Month.class, names = { "JANUARY", "MARCH", "MAY", "JULY", "AUGUST", "OCTOBER",
+            "DECEMBER" })
     public void testHas31Days(DateChecker.Month month) {
         assertTrue(dateChecker.has31Days(month));
 
     }
 
     @ParameterizedTest
-    @EnumSource(value = DateChecker.Month.class, names = {"FEBRUARY", "APRIL", "JUNE", "SEPTEMBER", "NOVEMBER"})
+    @EnumSource(value = DateChecker.Month.class, names = { "FEBRUARY", "APRIL", "JUNE", "SEPTEMBER", "NOVEMBER" })
     public void testDoesNotHave31Days(DateChecker.Month month) {
         assertFalse(dateChecker.has31Days(month));
 
